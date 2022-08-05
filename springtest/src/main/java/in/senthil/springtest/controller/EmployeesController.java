d5793e0e4fdebf64261c0e934f92f767dd3a0ba5
SENTHIL-7 <apsenthilkumar2001@gmail.com>
2022-08-05 22:07:42 +0530

    RestController is combined @Controller and @ResponseBody

--- springtest/src/main/java/in/senthil/springtest/controller/EmployeesController.java
+++ springtest/src/main/java/in/senthil/springtest/controller/EmployeesController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController// @Controller + @ResponseBody
+@RestController                              // @Controller + @ResponseBody
 public class EmployeesController {
     @RequestMapping(value="/employees" ,method=RequestMethod.GET) 
    
